41bdbd9e38b92618b9fe504166721e2ca6a9502b
hagleyson <hagleyson@gmail.com>
2021-12-16 12:24:32 -0300

    feat: add automatically generate number container

---
+++ dom.js
@@ -0,0 +1,34 @@
+(function (win, doc) {
+  //construtor
+  function DOM(no) {
+    if (!(this instanceof DOM)) return new DOM(no);
+    this.element = doc.querySelectorAll(no);
+  }
+  //função de adicionar listener
+  DOM.prototype.on = function on(e, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.addEventListener(e, callback, false);
+    });
+  };
+  //função de remover listener
+  DOM.prototype.off = function off(e, callback) {
+    Array.prototype.forEach.call(this.element, function (element) {
+      element.removeEventListener(e, callback, false);
+    });
+  };
+  //função para trazer o elemento
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  //map
+  DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+  };
+  //filter
+  DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+  };
+
+  win.DOM = DOM;
+})(window, document);
--- script.js
+++ script.js
@@ -1,148 +1,54 @@
-/*
-Já temos as funcionalidades de adicionar e remover um carro. Agora, vamos persistir esses dados, 
-salvando-os temporariamente na memória de um servidor.
-Nesse diretório do `challenge-32` tem uma pasta `server`. É um servidor simples, em NodeJS, para 
-que possamos utilizar para salvar as informações dos nossos carros.
-Para utilizá-lo, você vai precisar fazer o seguinte:
-- Via terminal, acesse o diretório `server`;
-- execute o comando `npm install` para instalar as dependências;
-- execute `node app.js` para iniciar o servidor.
-Ele irá ser executado na porta 3000, que pode ser acessada via browser no endereço: 
-`http://localhost:3000`
-O seu projeto não precisa estar rodando junto com o servidor. Ele pode estar em outra porta.
-As mudanças que você irá precisar fazer no seu projeto são:
-- Para listar os carros cadastrados ao carregar o seu projeto, faça um request GET no endereço
-`http://localhost:3000/car`
-- Para cadastrar um novo carro, faça um POST no endereço `http://localhost:3000/car`, enviando
-os seguintes campos:
-  - `image` com a URL da imagem do carro;
-  - `brandModel`, com a marca e modelo do carro;
-  - `year`, com o ano do carro;
-  - `plate`, com a placa do carro;
-  - `color`, com a cor do carro.
-Após enviar o POST, faça um GET no `server` e atualize a tabela para mostrar o novo carro cadastrado.
-Crie uma branch `challenge-32` no seu projeto, envie um pull request lá e cole nesse arquivo a URL
-do pull request.
-*/
-
 (function ($, doc) {
   "use strict";
   function app() {
-    var $title = $("[data-js='Title']");
-    var $image = $("[data-js='image']");
-    var $brandModel = $("[data-js='brandModel']");
-    var $year = $("[data-js='year']");
-    var $plate = $("[data-js='plate']");
-    var $color = $("[data-js='color']");
-    var $tbody = $("[data-js='tbody']");
+    var $desc = $("[data-js='desc']");
+    var $gameAtual = {};
+    var $numbers = $('[data-js="numbers"]');
+    var $SelectedNumber = [];
 
     return {
       init: function init() {
-        this.getInfoBussines();
+        this.getInfoGame(0);
         this.initEvents();
-        this.getCars();
       },
       initEvents: function initEvents() {
-        $('[data-js="form"]').on("submit", this.handleSubmit);
+        $('[data-js="lotofacil"]').on("click", function () {
+          app().handleClick(0);
+        });
+        $('[data-js="megasena"]').on("click", function () {
+          app().handleClick(1);
+        });
+        $('[data-js="lotomania"]').on("click", function () {
+          app().handleClick(2);
+        });
       },
-      handleSubmit: function handleSubmit(e) {
-        e.preventDefault();
-        var ajax = new XMLHttpRequest();
-        ajax.open("POST", "http://localhost:3000/car");
-        ajax.setRequestHeader(
-          "Content-type",
-          "application/x-www-form-urlencoded"
-        );
-        ajax.send(
-          `image=${$image.get()[0].value}&brandModel=${
-            $brandModel.get()[0].value
-          }&year=${$year.get()[0].value}&plate=${$plate.get()[0].value}&color=${
-            $color.get()[0].value
-          }`
-        );
-        console.log("começou a bagaceira");
-        ajax.onreadystatechange = function () {
-          if (ajax.readyState === 4) {
-            app().getCars();
-          }
-        };
+      handleClick: function handleClick(type) {
+        app().getInfoGame(type);
       },
-      getCars: function getCars() {
-        var ajax = new XMLHttpRequest();
-        ajax.open("GET", "http://localhost:3000/car");
-        ajax.send();
-        ajax.addEventListener(
-          "readystatechange",
-          function () {
-            if (ajax.readyState === 4 && ajax.status === 200) {
-              var response = JSON.parse(ajax.responseText);
-              app().listCars(response);
-            }
-          },
-          false
-        );
-      },
-      listCars: function listCars(cars) {
-        var fragment = document.createDocumentFragment();
-
-        if (!!cars[0]) {
-          cars.map((c) => {
-            var $tr = document.createElement("tr");
-            var $tdImage = document.createElement("td");
-            var $image = doc.createElement("img");
-            var $tdBrandModel = document.createElement("td");
-            var $tdYear = document.createElement("td");
-            var $tdPlate = document.createElement("td");
-            var $tdColor = document.createElement("td");
-            $image.setAttribute("src", c.image);
-            $tdImage.appendChild($image);
-            $tdBrandModel.textContent = c.brandModel;
-            $tdYear.textContent = c.year;
-            $tdPlate.textContent = c.plate;
-            $tdColor.textContent = c.color;
-            $tr.appendChild($tdImage);
-            $tr.appendChild($tdBrandModel);
-            $tr.appendChild($tdYear);
-            $tr.appendChild($tdPlate);
-            $tr.appendChild($tdColor);
-            $tr.appendChild(app().addButtonDelete());
-            fragment.appendChild($tr);
-            $tbody.get()[0].appendChild(fragment);
-          });
+      createFieldBet: function createFieldBet(range) {
+        var $fragment = document.createDocumentFragment();
+        for (let index = 0; index <= range; index++) {
+          var $div = doc.createElement("div");
+          var $h1 = doc.createElement("h1");
+          $h1.textContent = index;
+          $div.appendChild($h1);
+          $fragment.appendChild($div);
         }
+        $numbers.get()[0].innerHTML = "";
+        $numbers.get()[0].appendChild($fragment);
       },
-      addButtonDelete: function addButtonDelete() {
-        var $td = document.createElement("td");
-        var $delButton = document.createElement("button");
-
-        $delButton.textContent = "excluir";
-        $delButton.addEventListener(
-          "click",
-          function () {
-            var $tr = $td.parentNode;
-            // console.log($tr);
-            $tbody.get()[0].removeChild($tr);
-            console.log($tbody.appendChild);
-          },
-          false
-        );
-        $td.appendChild($delButton);
-        return $td;
-      },
-      deleteButton: function (e) {
-        e.preventDefault();
-      },
-
-      getInfoBussines: function getInfoBussines() {
+      getInfoGame: function getInfoGame(type) {
         var ajax = new XMLHttpRequest();
-        ajax.open("GET", "company.json");
+        ajax.open("GET", "games.json");
         ajax.send();
         ajax.addEventListener(
           "readystatechange",
           function () {
             if (ajax.readyState === 4 && ajax.status === 200) {
               var response = JSON.parse(ajax.responseText);
-              $title.get()[0].innerHTML = `${response.name} Tel: ${response.phone}`;
+              $gameAtual = response.types[type];
+              $desc.get()[0].innerHTML = $gameAtual.description;
+              app().createFieldBet($gameAtual.range);
             }
           },
           false
